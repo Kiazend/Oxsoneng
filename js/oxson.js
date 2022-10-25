9adb0d2cf3c61cd5c9e3e886cd6511b5081899ec
Kiazend <ahmad.slayer.00@gmail.com>
2022-10-26 00:53:01 +0500

    website completed

--- js/oxson.js
+++ js/oxson.js
@@ -1,1 +1,16 @@
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+ $(()=>{  //DOCUMENT ON LOAD
+
+$("#hamburger").on("click", function () {
+    $("#hamburger").hide(100);
+    $("#cross").slideDown(200);
+    $(".nav-item").slideDown(200);
+});
+
+$("#cross").on("click", function () {
+    $(".nav-item").slideUp(200);
+    $("#cross").slideUp(200);
+    $("#hamburger").show(100);
+});
+
+})
